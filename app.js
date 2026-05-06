0c2c5558a3d6d8d5d90da006fbb261c850c79c15
rovshanimamaliyev-coder <rovshan.imamaliyev@gmail.com>
2026-05-06 12:59:15 +0400

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,14 @@ function calculate() {
 calculate();
 
 // PWA Service Worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js');
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("sw.js")
+      .then(reg => {
+        console.log("Service Worker registered:", reg.scope);
+      })
+      .catch(err => {
+        console.log("Service Worker failed:", err);
+      });
+  });
 }
